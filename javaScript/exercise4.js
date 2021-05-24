ae03dad02fa6905de3bfa010c05c99bff67056a1
Kesava Karri <kkeshav1998@gmail.com>
2021-05-23 20:47:21 -0400

    various declaration function types in javaScript

---
+++ javaScript/exercise4.js
@@ -0,0 +1,8 @@
+var age = Number(prompt("Enter your age: "));
+if (age > 18) {
+    alert("Powering On. Enjoy the ride!");
+} else if (age === 18) {
+    alert("Congratulations on your first year of driving. Enjoy the ride!");
+} else {
+    alert("Sorry, you are too young to drive this car. Powering off");
+}
---
+++ javaScript/exercise5.js
@@ -0,0 +1,38 @@
+//Declaring function using Function declaration 
+function checkDriverAge() {
+    var age = Number(prompt("What is your age?"));
+    if (age > 18) {
+        return "Powering On. Enjoy the ride!";
+    } else if (age === 18) {
+        return "Congratulations on your first year of driving. Enjoy the ride!";
+    } else {
+        return "Sorry, you are too young to drive this car. Powering off";
+    }
+}
+
+alert(checkDriverAge());
+
+//Declaring function using Function expression
+var checkDriverAge2 = function() {
+    var age = Number(prompt("What is your age?"));
+    if (age > 18) {
+        return "Powering On. Enjoy the ride!";
+    } else if (age === 18) {
+        return "Congratulations on your first year of driving. Enjoy the ride!";
+    } else {
+        return "Sorry, you are too young to drive this car. Powering off";
+    }
+}
+
+//function using pre-defined age rather than prompting the user
+// function checkDriverAge(age) {
+//     if (age > 18) {
+//         return "Powering On. Enjoy the ride!";
+//     } else if (age === 18) {
+//         return "Congratulations on your first year of driving. Enjoy the ride!";
+//     } else {
+//         return "Sorry, you are too young to drive this car. Powering off";
+//     }
+// }
+
+// alert(checkDriverAge(22));
