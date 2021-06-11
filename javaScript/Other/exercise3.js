b5339225bb52720a8ab34dbc2f97219e3213f8af
Kesava Karri <kkeshav1998@gmail.com>
2021-06-11 13:28:00 -0400

    let, template strings,arrow functions

---
+++ javaScript/Other/exercise3.js
@@ -0,0 +1,70 @@
+// change everything below to the newer Javascript!
+
+// let + const
+let a = 'test'; //var a = 'test';
+const b = true; //var b = true;
+let c = 789; //var c = 789;
+a = 'test2';
+
+
+// Destructuring
+const person = {
+    firstName : "John",
+    lastName  : "Doe",
+    age       : 50,
+    eyeColor  : "blue"
+};
+
+// var firstName = person.firstName;
+// var lastName = person.lastName;
+// var age = person.age;
+// var eyeColor = person.eyeColor;
+const { firstName, lastName, eyeColor } = person;
+let { age } = person; // we use let as age changes w time & the other given properties of the object doesn't
+
+// Object properties
+const a = 'test'; //var a = 'test';
+const b = true; //var b = true;
+const c = 789; //var c = 789;
+
+// var okObj = {
+//   a: a,
+//   b: b,
+//   c: c
+// };
+const okObj = {
+    a, b, c
+}
+
+
+
+// Template strings
+// var message = "Hello " + firstName + " have I met you before? I think we met in " + city + " last summer no???";
+const message = `Hello ${firstName} have I met you before? I think we met in the ${city} last summer no???`;
+
+// default arguments
+// default age to 10;
+// function isValidAge(age) {
+//     return age
+// }
+
+const isValidAge = (age = 10) => age;
+
+// Symbol
+// Create a symbol: "This is my first Symbol"
+let sym = Symbol("This is my first symbol");
+
+// Arrow functions
+// function whereAmI(username, location) {
+//     if (username && location) {
+//         return "I am not lost";
+//     } else {
+//         return "I am totally lost!";
+//     }
+// }
+
+const whereAmI = (username, location) => {
+    return (username && location) ? "I am not lost" : "I am totally lost!";
+}
+
+// const whereAmI = (username, location) => (username && location) ? "I am not lost" : "I am totally lost";
