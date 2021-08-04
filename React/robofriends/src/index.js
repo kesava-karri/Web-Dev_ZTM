68f09f68948d693c1717408d4c0265da59ff71c0
Kesava Karri <kkeshav1998@gmail.com>
2021-08-04 19:57:32 -0400

    Card components

--- React/robofriends/src/index.js
+++ React/robofriends/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import Hello from './Hello.js';
 import 'tachyons';
-import reportWebVitals from './reportWebVitals';
+import Card from './Card.js';
+import { robots } from './robots.js';
 
-ReactDOM.render(<Hello dawg={'another prop'} greeting={'React ninja 9+4'}/>, document.getElementById('root'));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+	<div>
+		<Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+		<Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+		<Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+	</div>
+	, document.getElementById('root')
+);
