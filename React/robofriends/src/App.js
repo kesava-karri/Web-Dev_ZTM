973d33c30463f7b1d273b253e268a4e1b5707092
Kesava Karri <kkeshav1998@gmail.com>
2021-08-05 19:14:48 -0400

    searchBox functionality

--- React/robofriends/src/App.js
+++ React/robofriends/src/App.js
@@ -2,26 +2,30 @@ import React, { Component } from 'react';
 import CardArray from './CardArray';
 import { robots } from './robots';
 import SearchBox from './SearchBox';
+import './App.css';
 
 class App extends Component{
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      // robots: robots,
       searchfield: ''
     }
   }
 
-  onSearchChange(event) {
-    console.log(event.target.value);
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
   }
 
   render() {
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchfield)
+    })
     return(
       <div className='tc'>
-        <h1>Robots - Not Transformers</h1>
+        <h1 className='f1'>Robots - Not Transformers</h1>
         <SearchBox searchChange = {this.onSearchChange}/>
-        <CardArray robots={robots}/>
+        <CardArray robots={filteredRobots}/>
       </div>
     );
   }
