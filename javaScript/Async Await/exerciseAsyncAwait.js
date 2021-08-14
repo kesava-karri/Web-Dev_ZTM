deba799517408c138c27735bea128f86bc01b93e
Kesava Karri <kkeshav1998@gmail.com>
2021-08-14 14:32:50 -0400

    object spead operator

---
+++ javaScript/Async Await/exerciseAsyncAwait.js
@@ -0,0 +1,72 @@
+// Solve the below problems:
+
+// #1) Convert the below promise into async await
+fetch("https://jsonplaceholder.typicode.com/users/")
+  .then((response) => response.json())
+  .then(console.log);
+//---------------------------------------------\\
+async function fetchUsers() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users/');
+  const users = await response.json();
+  console.log(users);
+}
+
+// #2) ADVANCED: Update the function below from the video to also have
+// async await for this line: fetch(url).then(resp => resp.json())
+// So there shouldn't be any .then() calls anymore!
+// Don't get discouraged... this is a really tough one...
+const urls = [
+  "https://jsonplaceholder.typicode.com/users",
+  "https://jsonplaceholder.typicode.com/posts",
+  "https://jsonplaceholder.typicode.com/albums",
+];
+
+const getData = async function () {
+  const [users, posts, albums] = await Promise.all(
+    urls.map((url) => fetch(url).then((resp) => resp.json())),
+  );
+  console.log("users", users);
+  console.log("posts", posts);
+  console.log("albums", albums);
+};
+//---------------------------------------------\\
+const urls = [
+  "https://jsonplaceholder.typicode.com/users",
+  "https://jsonplaceholder.typicode.com/posts",
+  "https://jsonplaceholder.typicode.com/albums",
+];
+
+const getData1 = async function () {
+  const [users, posts, albums] = await Promise.all(urls.map(
+    async function URL(url) {
+      const temp_url = await fetch(url);
+      return temp_url.json();
+    }
+  ));
+  console.log("users", users);
+  console.log("posts", posts);
+  console.log("albums", albums);
+};
+
+// #3)Add a try catch block to the #2 solution in order to catch any errors. // Now, use the given array containing an invalid url, so you console.log  //your error with 'oooooops'.
+const urls = [
+  "https://jsonplaceholder.typicode.com/users",
+  "https://jsonplaceholdeTYPO.typicode.com/posts",
+  "https://jsonplaceholder.typicode.com/albums",
+];
+
+const getData1 = async function () {
+  try {
+    const [users, posts, albums] = await Promise.all(urls.map(
+      async function URL(url) {
+        const temp_url = await fetch(url);
+        return temp_url.json();
+      }
+    ));
+    console.log("users", users);
+    console.log("posts", posts);
+    console.log("albums", albums);
+  } catch (err) {
+    console.log('oooooops');
+  }
+};
---
+++ javaScript/Async Await/spread-operator (ES9).js
@@ -0,0 +1,9 @@
+//Object spread operator
+const MHA = {
+    deku: 23,
+    kacchan: 25,
+    todoroki: 24,
+    allMight: 35
+}
+
+const { deku, kacchan, ...rest } = MHA; //rest can be named anything
